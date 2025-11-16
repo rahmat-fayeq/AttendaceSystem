3af8e4865177e0e087e7175d3a664347bda8ad76
Rahmat <fayeq.rahmat@gmail.com>
2025-11-16 11:18:34 +0430

    enable light & dark support

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -19,24 +19,46 @@
                 <li><a>Item 3</a></li>
             </ul>
         </div>
-        <a class="btn btn-ghost text-xl">daisyUI</a>
+        <a class="btn btn-ghost text-xl" href="/">Attendance System</a>
     </div>
     <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
+            <li><a href="{{ route('devices.index') }}">Device</a></li>
             <li>
                 <details>
-                    <summary>Parent</summary>
+                    <summary>Registration</summary>
                     <ul class="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
+                        <li><a href="{{ route('students.index') }}">Students</a></li>
+                        <li><a href="#">Teacher</a></li>
                     </ul>
                 </details>
             </li>
-            <li><a>Item 3</a></li>
+            <li>
+                <details>
+                    <summary>Reports</summary>
+                    <ul class="p-2">
+                        <li><a href="{{ route('attendance.daily') }}">Daily</a></li>
+                    </ul>
+                </details>
+            </li>
         </ul>
     </div>
     <div class="navbar-end">
-        <a class="btn">Button</a>
+        <div class="flex items-center gap-4">
+            <div class="navbar-end">
+                <button id="theme-toggle" class="btn btn-ghost btn-circle">
+                    <span id="icon-light">🌞</span>
+                    <span id="icon-dark" class="hidden">🌙</span>
+                </button>
+            </div>
+            <a class="btn btn-sm btn-secondary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-log-out-icon lucide-log-out">
+                    <path d="m16 17 5-5-5-5" />
+                    <path d="M21 12H9" />
+                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                </svg></a>
+        </div>
     </div>
 </div>
